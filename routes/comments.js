ee89089c0b042af54c6f2c619d1aaafe7864667f
hanshank <hans@hanken.org>
2019-03-01 14:46:58 -0800

    Add lowdb

--- routes/comments.js
+++ routes/comments.js
@@ -4,17 +4,32 @@ const moment = require('moment');
 const shortid = require('shortid');
 const fs = require('fs').promises;
 const path = require('path');
+const lowdb = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 const commentData = require('../data');
+
+// create the db file if it doesn't exist
+// and seed it with data
+const adapter = new FileSync('db.json', {
+  defaultValue: { comments: commentData },
+});
+
+const db = lowdb(adapter);
 
 const router = express.Router();
 
+// get all comments
 router.get('/', (req, res) => {
+  const comments = db.get('comments').value();
   res.json(commentData);
 });
 
 // get a single comment by id
 router.get('/:id', (req, res) => {
-  const myComment = commentData.find(comment => comment.id === parseInt(req.params.id));
+  const myComment = db
+    .get('comments')
+    .find({ id: req.params.id })
+    .value();
   if (myComment) {
     res.json(myComment);
   } else {
@@ -50,23 +65,45 @@ router.post('/', (req, res) => {
     timestamp: moment().format(),
   };
 
-  commentData.push(newComment);
+  // add it to commentdata
 
-  res.status(201).json({ msg: 'Comment succesfully added!', comments: commentData });
+  db.get('comments')
+    .push(newComment)
+    .write();
+
+  res.status(201).json({
+    msg: 'Comment succesfully added!',
+    comments: db.get('comments').value()
+  });
 });
 
 // Update a comment
-router.patch('/:id', (req, res) => {
-  const myComment = commentData.find(comment => comment.id === parseInt(req.params.id));
+router.put('/:id', (req, res) => {
+  // const myComment = commentData.find(comment => comment.id === parseInt(req.params.id));
 
   if (!req.body.text) {
-    res.status(400).json({ msg: 'You can do better! Try adding some text next time...' });
-  } else if (!myComment) {
-    res.status(404).json({ msg: 'That comment does not exist dude!' });
+    return res.status(400).json({ msg: 'You can do better! Try adding some text next time...' });
   }
 
-  myComment.text = req.body.text;
-  myComment.updatedAt = moment().format();
+  if (
+    !db
+      .get('comments')
+      .find({ id: req.params.id })
+      .value()
+  ) {
+    res.status(404).json({ msg: 'That comment does not exist dude!' });
+  }
+  // else if (!myComment) {
+  //   res.status(404).json({ msg: 'That comment does not exist dude!' });
+  // }
+
+  db.get('comments')
+    .find({ id: req.params.id })
+    .assign({ text: req.body.text, updatedAt: moment().format() })
+    .write();
+
+  // myComment.text = req.body.text;
+  // myComment.updatedAt = moment().format();
 
   // myComment = {
   //   ...myComment,
@@ -74,23 +111,28 @@ router.patch('/:id', (req, res) => {
   //   updatedAt: moment().format(),
   // };
 
-  res.json({ comments: commentData });
+  res.json({ comments: db.get('comments') });
 
-  res.status(201).json({ msg: 'Comment succesfully added!', comments: commentData });
+  res.status(201).json({ msg: 'Comment succesfully added!', comments: db.get('comments') });
 });
 
 router.delete('/:id', (req, res) => {
-  const myComment = commentData.find(comment => comment.id === parseInt(req.params.id));
-
-  if (!myComment) {
+  if (
+    !db
+      .get('comments')
+      .find({ id: req.params.id })
+      .value()
+  ) {
     res.status(404).json({ msg: 'That comment does not exist dude!' });
   }
 
-  commentData.splice(commentData.id, 1);
+  db.get('comments')
+    .remove({ id: req.params.id })
+    .write();
 
   res.status(202).json({
-    msg: 'See you later sucker! That comment is out of here....',
-    comments: commentData,
+    msg: 'Comment succesfully deleted',
+    comments: db.get('comments').value()
   });
 });
 
